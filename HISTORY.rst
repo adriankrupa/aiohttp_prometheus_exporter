2d92f0457a1b3ca5274cb5fb23e9f55b730f1d24
Adrian Krupa <adrian.krupa91@gmail.com>
2020-04-15 17:18:08 +0200

    Configure linter (#7)
    * Configure linter
    
    * Run docs and lint on every env

--- HISTORY.rst
+++ HISTORY.rst
@@ -1,6 +1,11 @@
-=======
 History
 =======
+
+0.2.4 (2020-04-07)
+------------------
+
+* Fixed building problems
+* Configured black linter
 
 0.1.0 (2020-04-07)
 ------------------
--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
     include_package_data=True,
     keywords='aiohttp_prometheus_exporter',
     name='aiohttp_prometheus_exporter',
